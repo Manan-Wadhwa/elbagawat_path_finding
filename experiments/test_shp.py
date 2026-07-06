c6d8ea14aba77004ece7f0f550dd1278b67ad223
Manan-Wadhwa <wadhwamanan11@outlook.com>
2026-07-06 21:30:20 +0530

    path finding only

--- experiments/test_shp.py
+++
@@ -1,8 +0,0 @@
-import os
-import geopandas as gpd
-import pandas as pd
-
-shp_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "BaseSiteCAD", "130_BuildingFootprintsVectorData", "BuildingTracesCurrent", "Buildings_Mask.shp")
-gdf = gpd.read_file(shp_path)
-print(gdf.columns)
-print(gdf.head())
